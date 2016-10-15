9484296149005fbaecd2912ea633834d40bb1c7f
RexRed <a123ggg09@gmail.com>
2016-10-14 19:48:57 -0500

    Add a test Logger

--- FakePlayers.php
+++ FakePlayers.php
@@ -18,6 +18,7 @@ $this->getServer()->getPluginManager()->registerEvents($this, $this);
 
 public function onQuery(QueryRegenerateEvent $e){
 $e->setPlayerCount(mt_rand(5,1000));
+$this->getLogger->notice("Update Player Count~");
 }
 
 }
