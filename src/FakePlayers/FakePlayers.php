e79142b7a15e20e78247d285fede8792e85094f5
SuperXingKong <1912003473@qq.com>
2017-01-16 06:12:17 -0600

    fix some awesome bugs
    my mistakes!

--- src/FakePlayers/FakePlayers.php
+++ src/FakePlayers/FakePlayers.php
@@ -13,8 +13,9 @@ use pocketmine\command\CommandSender;
 class FakePlayers extends PluginBase implements Listener{
 
 public function onEnable(){
-$this->getLogger()->notice("FakePlayers plugin made by RexRed");
+$this->getLogger()->notice("FakePlayers plugin made by RexRed & maintenanced by SuperXingKong");
 $this->getServer()->getPluginManager()->registerEvents($this, $this);
+@mkdir($this->getDataFolder(),0777,true);
 $this->saveDefaultConfig();
 $this->reloadConfig();
 }
@@ -28,18 +29,24 @@ $this->getLogger()->notice("Update Player Count~\nNow PlayerCount : $Count");
 }
 
 public function onCommand(CommandSender $sender, Command $cmd, $label, array $arg){
-if (strtolower($cmd–>getName()) == 'smaxc'){
+if (strtolower($cmd->getName()) == 'smaxc'){
 if (isset($arg[0])){
-$this–>getConfig()–>set('max',"$arg[0]");
+$this->getConfig()->set('max',"$arg[0]");
+$this->getConfig()->save();
+$sender->sendMessage("成功设置最大人数:$arg[0]");
+return true;
 }else{
-$sender–>sendMessage("缺少参数");
+$sender->sendMessage("缺少参数");
 }
 }
-if (strtolower($cmd–>getName()) == 'sminc'){
+if (strtolower($cmd->getName()) == 'sminc'){
 if (isset($arg[0])){
-$this–>getConfig()–>set('min',"$arg[0]");
+$this->getConfig()->set('min',"$arg[0]");
+$this->getConfig()->save();
+$sender->sendMessage("成功设置最小人数:$arg[0]");
+return true;
 }else{
-$sender–>sendMessage("缺少参数");
+$sender->sendMessage("缺少参数");
 }
 }
 }
